aff84020b779882e7276fee8255336442ca5fcd9
mattiadebonis <mattia.debonis@gmail.com>
2021-04-26 17:29:51 +0200

    Genera e mostra password

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,4 @@ var nomeUtente = prompt()
 var cognomeUtente = prompt()
 var colorePreferitoUtente = prompt()
 
+var passwordUtente = document.getElementById('password').innerHTML = nomeUtente + cognomeUtente + colorePreferitoUtente
