6015be9f3b76501f503b35f6d97accb446bab260
mattiadebonis <mattia.debonis@gmail.com>
2021-04-26 18:48:19 +0200

    css

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,7 @@
-var nomeUtente = prompt("Inserisci il tuo nome")
-var cognomeUtente = prompt("Inserisci il tuo cognome")
-var colorePreferitoUtente = prompt("Inserisci il tuo colore preferito")
 
-var passwordUtente = document.getElementById('password').innerHTML = nomeUtente + cognomeUtente + colorePreferitoUtente + "21"
+var nomeUtente = prompt("Inserisci il tuo nome") // variabile nome
+var cognomeUtente = prompt("Inserisci il tuo cognome") //variabile cognome
+var colorePreferitoUtente = prompt("Inserisci il tuo colore preferito") //variabile colore preferito
+
+
+var passwordUtente = document.getElementById('password').innerHTML = nomeUtente + cognomeUtente + colorePreferitoUtente + "21" //nuova password 
